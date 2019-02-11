e86eb5b3aa38ce578998942dd92491e5b89f3de6
alaric <mirandaalaric@gmail.com>
2019-02-11 14:08:49 +0530

    initial commit

---
+++ EcommBackend/src/main/java/com/ecomm/dao/UserDao.java
@@ -0,0 +1,14 @@
+package com.ecomm.dao;
+
+import java.util.List;
+
+import com.ecomm.model.User;
+
+public interface UserDao {
+	public boolean add(User userId);
+	public boolean delete(User userId);
+	public boolean update(User userId);
+	public List<User> listUsers();
+	public User getUser(int userId);
+
+}
